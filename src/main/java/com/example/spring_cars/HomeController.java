07fa09ec0c89dfcc83aad836dbadd0d82cde56e2
Vigor Lundkvist <vigor.lundkvist@live.se>
2023-10-19 16:26:33 +0200

    Spring application that can add cars to DB with form

--- src/main/java/com/example/spring_cars/HomeController.java
+++ src/main/java/com/example/spring_cars/HomeController.java
@@ -1,12 +1,38 @@
 package com.example.spring_cars;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String getHome(){
+    @GetMapping("/") // This method shows the start page
+    public String getHome(Model model){
+
+        // Connect to the db
+        Database db = new Database();
+
+        // Get all cars from databse
+        List<Car> cars = db.getCars();
+
+        // Send cars to template
+        model.addAttribute("cars", cars);
+
+        // Show home template
         return "home";
+    }
+
+    @PostMapping("/")
+    public String postHome(String make, String model, int year){
+        Car car = new Car(make, model, year);
+
+        Database db = new Database();
+
+        db.saveCar(car);
+        return "redirect:/";
     }
 }
